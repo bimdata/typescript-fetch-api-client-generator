0f8636bfb0103efaeb2a1f8b7a855f8abf454ae6
Antoine Mineau <antoine@bimdata.io>
2021-01-18 15:46:36 +0100

    add semantic config file

---
+++ config/release.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-useless-escape */
+module.exports = {
+    verifyConditions: ['@semantic-release/npm', '@semantic-release/github'],
+    analyzeCommits: {
+        preset: 'eslint',
+        releaseRules: [
+            {tag: 'MINOR', release: 'minor'},
+            {tag: 'PATCH', release: 'patch'},
+            {tag: 'MAJOR', release: 'major'},
+        ],
+        parserOpts: {
+            headerCorrespondence: [
+            'tag',
+            'message',
+            ],
+        },
+    },
+    prepare: ['@semantic-release/npm'],
+    publish: ['@semantic-release/npm', '@semantic-release/github'],
+  };
+  
