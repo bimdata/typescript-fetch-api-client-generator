39c766ac76a4176c0e5bd6b93f4fe16da3469905
Antoine Mineau <antoine@bimdata.io>
2022-07-06 15:13:41 +0200

    MAJOR: Parameters and requesters in snake_case

--- generate-package.sh
+++ generate-package.sh
@@ -2,23 +2,23 @@
 
 GENERATOR=$1
 if [ -z "$GENERATOR" ]; then
-  GENERATOR=openapi-generator.jar
+    GENERATOR=openapi-generator.jar
 fi
 if [ ! -f $GENERATOR ]; then
-  >&2 echo "Generator jar not found."; exit 1
+    >&2 echo "Generator jar not found."; exit 1
 fi
 
 INPUT=$2
 if [ -z "$INPUT" ]; then
-  INPUT=swagger.json
+    INPUT=swagger.json
 fi
 if [ ! -f $INPUT ]; then
-  >&2 echo "Swagger file not found."; exit 1
+    >&2 echo "Swagger file not found."; exit 1
 fi
 
 VERSION=$3
 if [ -z "$VERSION" ]; then
-  VERSION=0.0.1
+    VERSION=0.0.1
 fi
 
 OUTPUT=./package
@@ -28,13 +28,15 @@ rm -rf $OUTPUT/src/runtime.ts $OUTPUT/src/apis $OUTPUT/src/models $OUTPUT/dist $
 # See https://openapi-generator.tech/docs/generators/typescript-fetch
 # for a list of available additional properties.
 java \
-  -jar $GENERATOR generate \
-  -g typescript-fetch \
-  -i $INPUT \
-  -o $OUTPUT \
-  --additional-properties \
+-jar $GENERATOR generate \
+-g typescript-fetch \
+-i $INPUT \
+-o $OUTPUT \
+--additional-properties \
 allowUnicodeIdentifiers=true,\
 supportsES6=true,\
+modelPropertyNaming=original,\
+paramNaming=original,\
 npmName=@bimdata/typescript-fetch-api-client,\
 useSingleRequestParameter=false,\
 npmVersion=$VERSION
